0d35dfcec134f4d6afccc15f0426590c0bb8265d
JonathonW <79091107+JDWilks@users.noreply.github.com>
2021-08-27 16:17:36 +0100

    test 1 on one card done

--- src/components/PokemonCard.test.js
+++ src/components/PokemonCard.test.js
@@ -1,3 +1,18 @@
+import { expect } from "@jest/globals";
+import "@testing-library/jest-dom";
+import { render, screen, logRoles } from "@testing-library/react";
+import PokemonCard from "./PokemonCard";
+import data from "../data.json";
+
+describe("Pokemon Name Heading", () => {
+  it("should have a h2 heading of ‘Pokemon Name’", () => {
+    render(<PokemonCard pokemon={data.pokemon[0]} />);
+    const headingH2 = screen.getByRole("heading", { name: "Bulbasaur" });
+    expect(headingH2).toBeInTheDocument();
+    logRoles(document.body);
+  });
+});
+
 // Test a single pokemon card:
 //  - heading
 //  - image
